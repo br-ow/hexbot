009c6dd7d1d197158aee1886fb385c8ae2b11cc1
Brifactor <31079664+brifactor@users.noreply.github.com>
2018-09-12 18:00:57 -0400

    Basic ping-pong bot with config file

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+  A ping pong bot, whenever you send "ping", it replies "pong".
+*/
+
+// Imports the discord.js module
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+// The ready event is vital, it means that your bot will only start reacting to information
+// from Discord _after_ ready is emitted
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(config.prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(config.prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+});
+
+// Log our bot in
+client.login(config.token);
