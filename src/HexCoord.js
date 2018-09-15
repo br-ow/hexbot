dd5165a90dffdd383b3402b8af745ae96441a81b
Brifactor <31079664+brifactor@users.noreply.github.com>
2018-09-15 01:22:09 -0400

    Moved source code into src folder. Added cubic and offset hex coordinates, with some basic functions. Offset coordinates only have functions to convert to and from cubic coordinates; the rest of the operations are handled with cubic coordinates.

---
+++ src/HexCoord.js
@@ -0,0 +1,89 @@
+/* Cubic hex coordinates
+ * Author: Brifactor
+ * Making heavy use of:
+ * https://www.redblobgames.com/grids/hexagons/implementation.html
+ */
+
+const HEX_DIR; //Hex directions using cubic coordinates
+
+//Cubic hex coordinates
+//all three coordinates must have a sum of zero.
+function HexCoord(q, r, s) {
+    if (Math.round(q + r + s) !== 0) {
+       throw "q + r + s must be 0";
+    }
+    else {
+       this.q = q;
+       this.r = r;
+       this.s = s;
+       return this;
+    }
+}
+
+//Basic functions for manipulating HexCoords
+function hex_add(a, b) {
+    return HexCoord(a.q + b.q, a.r + b.r, a.s + b.s);
+}
+
+function hex_subtract(a, b) {
+    return HexCoord(a.q - b.q, a.r - b.r, a.s - b.s);
+}
+
+function hex_scale(a, k) {
+    return HexCoord(a.q * k, a.r * k, a.s * k);
+}
+
+function hex_rotate_left(a) {
+    return HexCoord(-a.s, -a.q, -a.r);
+}
+
+function hex_rotate_right(a) {
+    return HexCoord(-a.r, -a.s, -a.q);
+}
+
+//Initial definition now that HexCoord has been defined
+/* Since we're using FLAT-TOPPED hexes, directions are:
+ * 0 = NE;  1 = SE;  2 = S
+ * 3 = SW;  4 = NW;  5 = N
+ */
+HEX_DIR = [HexCoord(1, 0, -1), HexCoord(1, -1, 0), HexCoord(0, -1, 1),
+ HexCoord(-1, 0, 1), HexCoord(-1, 1, 0), HexCoord(0, 1, -1)];
+
+function hex_direction(direction) {
+    return HEX_DIR[direction];
+}
+
+//Find neighbor in Direction
+function hex_neighbor(coord, direction) {
+    return hex_add(coord, hex_direction(direction));
+}
+
+function hex_length(coord) {
+    return ((Math.abs(coord.q) + Math.abs(coord.r) + Math.abs(coord.s)) / 2);
+}
+
+//Distance between two hexes
+function hex_distance(a, b) {
+    return hex_length (hex_subtract(a, b));
+}
+
+//round a cube coordinate to the nearest integer (hex) coordinate
+function hex_round(h) {
+    var qi = Math.round(h.q);
+    var ri = Math.round(h.r);
+    var si = Math.round(h.s);
+    var q_diff = Math.abs(qi - h.q);
+    var r_diff = Math.abs(ri - h.r);
+    var s_diff = Math.abs(si - h.s);
+
+    if (q_diff > r_diff && q_diff > s_diff) {
+        qi = -ri - si;
+    }
+    else if (r_diff > s_diff) {
+        ri = -qi - si;
+    }
+    else {
+        si = -qi - ri;
+    }
+    return HexCoord(qi, ri, si);
+}
---
+++ src/OffsetCoord.js
@@ -0,0 +1,43 @@
+/* Offset hex coordinates
+ * Only used for converting to and from world generator
+ * Author: Brifactor
+ * Making heavy use of:
+ * https://www.redblobgames.com/grids/hexagons/implementation.html
+ */
+
+//include
+const Hex = require('./HexCoord.js');
+
+//constants
+const EVEN = 1; //What TextMapper currently uses!
+const ODD = -1;
+const OFFSET = EVEN; //since it's not going to change and would break otherwise
+
+ function OffsetCoord(col, row) {
+    this.col = col;
+    this.row = row;
+    return this;
+ }
+
+
+function qoffset_from_cube(offset, h) {
+    var col = h.q;
+    var row = h.r + (h.q + offset * (h.q & 1)) / 2;
+    return OffsetCoord(col, row);
+}
+
+//Wrapper function because offset is likely to be predeterimined
+function offs_from_cube(h) {
+    return qoffset_from_cube(OFFSET, h);
+}
+
+function qoffset_to_cube(offset, h) {
+    var q = h.col;
+    var r = h.row - (h.col + offset * (h.col & 1)) / 2;
+    var s = -q - r;
+    return HexCoord(q, r, s);
+}
+
+function offs_to_cube(h) {
+    return qoffset_to_cube(OFFSET, h);
+}
