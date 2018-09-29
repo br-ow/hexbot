551ce7dd8af0f687d0de41aed5dc1f848133e9dd
Brifactor <31079664+brifactor@users.noreply.github.com>
2018-09-28 22:49:45 -0400

    Refactoring: HexCoord is now a class, and accompanying functions are class functions. Adjusted tests accordingly. Added tests for distance function.

--- src/HexCoord.js
+++ src/HexCoord.js
@@ -3,103 +3,114 @@
  * Making heavy use of:
  * https://www.redblobgames.com/grids/hexagons/implementation.html
  */
+"use strict";
 
-// const HEX_DIR; //Hex directions using cubic coordinates
+const math = require('mathjs');
+
 
 //Cubic hex coordinates
 //all three coordinates must have a sum of zero.
-exports.HexCoord = function(q, r, s) {
-    if (Math.round(q + r + s) !== 0) {
-        throw "q + r + s must be 0";
-    }
-    else {
+class HexCoord {
+    constructor(q, r, s) {
         this.q = q;
         this.r = r;
         this.s = s;
-        this.equals = function(other_coord) {
+
+        if(math.round(q + r + s) !== 0) {
+            throw "q + r + s must be 0";
+        }
+    }
+    equals(other_coord) {
             return ((other_coord.q === this.q ) && 
             (other_coord.r === this.r) && 
             (other_coord.s === this.s));
-        };
-        //TODO: Equals, hash code
-        return this;
     }
-};
 
-//Basic functions for manipulating HexCoords
-exports.add = function(a, b) {
-    return exports.HexCoord(a.q + b.q, a.r + b.r, a.s + b.s);
-};
-
-exports.subtract = function(a, b) {
-    return exports.HexCoord(a.q - b.q, a.r - b.r, a.s - b.s);
-};
-
-exports.scale = function(a, k) {
-    return exports.HexCoord(a.q * k, a.r * k, a.s * k);
-};
-
-exports.rotate_left = function(a) {
-    return exports.HexCoord(-a.s, -a.q, -a.r);
-};
-
-exports.rotate_right = function(a) {
-    return exports.HexCoord(-a.r, -a.s, -a.q);
-};
-
-/* Initial definition now that HexCoord has been defined
- * Since we're using FLAT-TOPPED hexes, directions are :
- * 0 = NE;  1 = SE;  2 = S
- * 3 = SW;  4 = NW;  5 = N
- */
-const HEX_DIR = [exports.HexCoord(1, 0, -1), exports.HexCoord(1, -1, 0),
- exports.HexCoord(0, -1, 1), exports.HexCoord(-1, 0, 1), exports.HexCoord(-1, 1, 0),
-  exports.HexCoord(0, 1, -1)];
-
-exports.direction = function(direction) {
-    return HEX_DIR[direction];
-};
-
-//Find neighbor in Direction
-exports.neighbor = function(coord, direction) {
-    return exports.hex_add(coord, hex_direction(direction));
-};
-
-exports.length = function(coord) {
-    return ((Math.abs(coord.q) + Math.abs(coord.r) + Math.abs(coord.s)) / 2);
-};
-
-//Distance between two hexes
-exports.distance = function(a, b) {
-    return exports.hex_length (hex_subtract(a, b));
-};
-
-//round a cube coordinate to the nearest integer (hex) coordinate
-exports.round = function(h) {
-    var qi = Math.round(h.q);
-    var ri = Math.round(h.r);
-    var si = Math.round(h.s);
-    var q_diff = Math.abs(qi - h.q);
-    var r_diff = Math.abs(ri - h.r);
-    var s_diff = Math.abs(si - h.s);
-
-    if (q_diff > r_diff && q_diff > s_diff) {
-        qi = -ri - si;
+    //Basic functions for manipulating HexCoords
+    add(b) {
+        return new HexCoord(this.q + b.q, this.r + b.r, this.s + b.s);
     }
-    else if (r_diff > s_diff) {
-        ri = -qi - si;
-    }
-    else {
-        si = -qi - ri;
-    }
-    return exports.HexCoord(qi, ri, si);
-};
 
-//Shift a HexCoord x distance in y direction
-exports.shift = function(h, dir, dist) {
-    //Make a copy of the direction so we don't mess it up
-    var dir_ref = exports.hex_direction(dir);
-    var direction = exports.HexCoord(dir_ref.q, dir_ref.r, dir_ref.s);
+    subtract(b) {
+        var qi = this.q - b.q;
+        var ri = this.r - b.r;
+        var si = this.s - b.s;
+        var answer = new HexCoord(qi, ri, si);
+        return answer;
+    }
 
-    return exports.hex_add(h, hex_scale(direction, dist));
-};
+    scale(k) {
+        return new HexCoord(this.q * k, this.r * k, this.s * k);
+    }
+
+    rotate_left() {
+        return new HexCoord(-this.s, -this.q, -this.r);
+    }
+
+    rotate_right() {
+        return new HexCoord(-this.r, -this.s, -this.q);
+    }
+
+    direction(direction) {
+        /* Initial definition now that HexCoord has been defined
+         * Since we're using FLAT-TOPPED hexes, directions are :
+         * 0 = NE;  1 = SE;  2 = S
+         * 3 = SW;  4 = NW;  5 = N
+         */
+        
+        const dirs = [new HexCoord(1, 0, -1), new HexCoord(1, -1, 0),
+         new HexCoord(0, -1, 1), new HexCoord(-1, 0, 1), new HexCoord(-1, 1, 0),
+          new HexCoord(0, 1, -1)];
+        return dirs[direction];
+    }
+
+    //Find neighbor in Direction
+    neighbor(direction) {
+        return this.add(this.direction(direction));
+    }
+
+    length() {
+        return ((math.abs(this.q) + math.abs(this.r) + math.abs(this.s))/2);
+    }
+
+    //Distance between two hexes
+    distance(b) {
+        var difference = this.subtract(b);
+        return difference.length();
+    }
+
+    //round a cube coordinate to the nearest integer (hex) coordinate
+    round() {
+        var qi = math.round(this.q);
+        var ri = math.round(this.r);
+        var si = math.round(this.s);
+        var q_diff = math.abs(qi - this.q);
+        var r_diff = math.abs(ri - this.r);
+        var s_diff = math.abs(si - this.s);
+
+        if (q_diff > r_diff && q_diff > s_diff) {
+            qi = -ri - si;
+        }
+        else if (r_diff > s_diff) {
+            ri = -qi - si;
+        }
+        else {
+            si = -qi - ri;
+        }
+        return new HexCoord(qi, ri, si);
+    }
+
+    //Shift a HexCoord x distance in y direction
+    shift(dir, dist) {
+        //Make a copy of the direction so we don't mess it up
+        var dir_ref = this.direction(dir);
+        var direction = new HexCoord(dir_ref.q, dir_ref.r, dir_ref.s);
+
+        return this.add(direction.scale(dist));
+    }
+
+}//end class
+
+
+
+exports.HexCoord = HexCoord;
--- test/test.js
+++ test/test.js
@@ -17,103 +17,141 @@ describe('Array', function() {
 describe('HexCoord', function() {
     describe('#this.q', function() {
         it('should retrieve q value of coordinate', function() {
-            assert.equal((hex_coord.HexCoord(1, 2, -3)).q, 1);
+            assert.equal((new hex_coord.HexCoord(1, 2, -3)).q, 1);
         });
     });
 
     describe('#this.q 2', function() {
         it('should retrieve q value of coordinate', function() {
-            assert.equal(hex_coord.HexCoord(-3, 2, 1).q, -3);
+            assert.equal(new hex_coord.HexCoord(-3, 2, 1).q, -3);
         });
     });
 
     describe('#this.r', function() {
         it('should retrieve r value of coordinate', function() {
-            assert.equal(hex_coord.HexCoord(1, 2, -3).r, 2);
+            assert.equal(new hex_coord.HexCoord(1, 2, -3).r, 2);
         });
     });
 
     describe('#this.r 2', function() {
         it('should retrieve r value of coordinate', function() {
-            assert.equal(hex_coord.HexCoord(1, -3, 2).r, -3);
+            assert.equal(new hex_coord.HexCoord(1, -3, 2).r, -3);
         });
     });
 
     describe('#this.s', function() {
         it('should retrieve s value of coordinate', function() {
-            assert.equal(hex_coord.HexCoord(1, 2, -3).s, -3);
+            assert.equal(new hex_coord.HexCoord(1, 2, -3).s, -3);
         });
     });
 
     describe('#this.s 2', function() {
         it('should retrieve s value of coordinate', function() {
-            assert.equal(hex_coord.HexCoord(-3, 2, 1).s, 1);
+            assert.equal(new hex_coord.HexCoord(-3, 2, 1).s, 1);
         });
     });
 
     describe('#add', function() {
         it('add two HexCoords together', function() {
-            var starting_spot = hex_coord.HexCoord(1, 2, -3);
-            var add_dir = hex_coord.HexCoord(1, 0, -1);
-            assert.equal(hex_coord.add(starting_spot, add_dir), hex_coord.HexCoord(2, 2, -4));
+            var starting_spot = new hex_coord.HexCoord(1, 2, -3);
+            var add_dir = new hex_coord.HexCoord(1, 0, -1);
+            assert.equal((starting_spot.add(add_dir)).equals(new hex_coord.HexCoord(2, 2, -4)), true);
         });
     });
 
     describe('#add 2', function() {
         it('add two HexCoords together', function() {
-            var starting_spot = hex_coord.HexCoord(1, 2, -3);
-            var add_dir = hex_coord.HexCoord(-1, 0, 1);
-            assert.equal(hex_coord.add(starting_spot, add_dir), hex_coord.HexCoord(0, 2, -2));
+            var starting_spot = new hex_coord.HexCoord(1, 2, -3);
+            var add_dir = new hex_coord.HexCoord(-1, 0, 1);
+            assert.equal(starting_spot.add(add_dir).equals(new hex_coord.HexCoord(0, 2, -2)), true);
         });
     });
 
     describe('#subtract', function() {
         it('subtract two HexCoords', function() {
-            var starting_spot = hex_coord.HexCoord(1, 2, -3);
-            var sub_dir = hex_coord.HexCoord(1, 0, -1);
-            assert.equal(hex_coord.subtract(starting_spot, sub_dir), hex_coord.HexCoord(0, 2, -2));
+            var starting_spot = new hex_coord.HexCoord(1, 2, -3);
+            var sub_dir = new hex_coord.HexCoord(1, 0, -1);
+            assert.equal(starting_spot.subtract(sub_dir).equals(new hex_coord.HexCoord(0, 2, -2)), true);
         });
     });
 
     describe('#subtract 2', function() {
         it('subtract two HexCoords', function() {
-            var starting_spot = hex_coord.HexCoord(1, 2, -3);
-            var sub_dir = hex_coord.HexCoord(-1, 0, 1);
-            assert.equal(hex_coord.subtract(starting_spot, sub_dir), hex_coord.HexCoord(2, 2, -4));
+            var starting_spot = new hex_coord.HexCoord(1, 2, -3);
+            var sub_dir = new hex_coord.HexCoord(-1, 0, 1);
+            assert.equal(starting_spot.subtract(sub_dir).equals(new hex_coord.HexCoord(2, 2, -4)), true);
         });
     });
 
     describe('#scale', function() {
         it('multiply a HexCoord', function() {
-            var starting_spot = hex_coord.HexCoord(1, 2, -3);
-            assert.equal(hex_coord.scale(starting_spot, 2), hex_coord.HexCoord(2, 4, -6));
+            var starting_spot = new hex_coord.HexCoord(1, 2, -3);
+            assert.equal(starting_spot.scale(2).equals(new hex_coord.HexCoord(2, 4, -6)), true);
         });
     });
 
     describe('#scale 2', function() {
         it('multiply a HexCoord', function() {
-            var starting_spot = hex_coord.HexCoord(1, 2, -3);
-            assert.equal(hex_coord.scale(starting_spot, 3), hex_coord.HexCoord(3, 6, -9));
+            var starting_spot = new hex_coord.HexCoord(1, 2, -3);
+            assert.equal(starting_spot.scale(3).equals(new hex_coord.HexCoord(3, 6, -9)), true);
         });
     });
 
     describe('#rotate_left', function() {
         it('rotate HexCoord to the left', function() {
-            var starting_spot = hex_coord.HexCoord(1, 2, -3);
-            assert.equal(hex_coord.rotate_left(starting_spot), hex_coord.HexCoord(3, -1, -2));
+            var starting_spot = new hex_coord.HexCoord(1, 2, -3);
+            assert.equal(starting_spot.rotate_left().equals(new hex_coord.HexCoord(3, -1, -2)), true);
         });
     });
 
     describe('#rotate_right', function() {
         it('rotate HexCoord to the right', function() {
-            var starting_spot = hex_coord.HexCoord(1, 2, -3);
-            assert.equal(hex_coord.rotate_right(starting_spot), hex_coord.HexCoord(-2, 3, -1));
+            var starting_spot = new hex_coord.HexCoord(1, 2, -3);
+            assert.equal(starting_spot.rotate_right().equals(new hex_coord.HexCoord(-2, 3, -1)), true);
         });
     });
 
     describe('#direction', function() {
         it('get a direction', function() {
-            assert.equal(hex_coord.direction(4), hex_coord.HexCoord(-1, 1, 0));
+            assert.equal(new hex_coord.HexCoord(0,0,0).direction(4).equals(new hex_coord.HexCoord(-1, 1, 0)), true);
+        });
+    });
+
+    describe('#neighbor', function() {
+        it('get a neighbor', function() {
+            debugger;
+            var starting_spot = new hex_coord.HexCoord(1, -2, 1);
+            assert.equal(starting_spot.neighbor(1).equals(new hex_coord.HexCoord(2, -3, 1)), true);
+        });
+    });
+
+    describe('#length', function() {
+        it('distance from hex to center?', function() {
+            var starting_spot = new hex_coord.HexCoord(4, -4, 0);
+            assert.equal(starting_spot.length(), 4);
+        });
+    });
+
+    describe('#length 2', function() {
+        it('distance from hex to center?', function() {
+            var starting_spot = new hex_coord.HexCoord(3, 2, -5);
+            assert.equal(starting_spot.length(), 5);
+        });
+    });
+
+    describe('#distance', function() {
+        it('distance between two hexes', function() {
+            var starting_spot = new hex_coord.HexCoord(1, -2, 1);
+            var ending_spot = new hex_coord.HexCoord(1, 2, -3);
+            assert.equal(starting_spot.distance(ending_spot), 4);
+        });
+    });
+
+    describe('#distance 2', function() {
+        it('distance between two hexes', function() {
+            var starting_spot = new hex_coord.HexCoord(1, -2, 1);
+            var ending_spot = new hex_coord.HexCoord(1, 1, -2);
+            assert.equal(starting_spot.distance(ending_spot), 3);
         });
     });
 
